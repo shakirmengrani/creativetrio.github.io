0216b3051a09217fd2f9b4432de42eba74e4497b
Shakir Mengrani <shakir.mengrani@gmail.com>
2021-07-09 14:10:20 +0500

    page

---
+++ _next/static/XMxe-6keKoPVcEZ4GYE04/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-338bb464a50e81ec4be9.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4a90ec492f28d3deeff9.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/webpack/pages/index.b853b1db8fce9c4ec397.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./data/Home.json":
+/*!************************!*\
+  !*** ./data/Home.json ***!
+  \************************/
+/*! exports provided: title, services, skills, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"title\\\":\\\"Netsolonline\\\",\\\"services\\\":[{\\\"label\\\":\\\"E-Commerce Solutions\\\",\\\"packages\\\":[{\\\"title\\\":\\\"Bronze\\\",\\\"features\\\":[\\\"1 domain with 2GB space for year\\\",\\\"Admin Panel\\\",\\\"Theme installation\\\",\\\"First 200 product entries\\\"],\\\"price\\\":\\\"PKR 10,000 / month\\\"},{\\\"title\\\":\\\"Silver\\\",\\\"features\\\":[\\\"1 domain with 5GB space for year\\\",\\\"Admin Panel\\\",\\\"Theme installation\\\",\\\"First 200 product entries\\\",\\\"Theme & plugin customization\\\",\\\"3rd party integration\\\"],\\\"price\\\":\\\"PKR 25,000 / month\\\"},{\\\"title\\\":\\\"Gold\\\",\\\"features\\\":[\\\"1 domain with 10GB space for year\\\",\\\"Admin Panel\\\",\\\"Theme installation\\\",\\\"First 200 product entries\\\",\\\"Theme & plugin customization\\\",\\\"3rd party integration\\\",\\\"Mobile App (Android & IOS)\\\"],\\\"price\\\":\\\"PKR 40,000 / month\\\"},{\\\"title\\\":\\\"Custom\\\"}]},{\\\"label\\\":\\\"Mobile App\\\",\\\"packages\\\":[{\\\"title\\\":\\\"Starter\\\"},{\\\"title\\\":\\\"Cross Platform\\\"},{\\\"title\\\":\\\"Native\\\"},{\\\"title\\\":\\\"Custom\\\"}]},{\\\"label\\\":\\\"Cloud Solutions\\\",\\\"packages\\\":[{\\\"title\\\":\\\"Bronze\\\"},{\\\"title\\\":\\\"Silver\\\"},{\\\"title\\\":\\\"Gold\\\"},{\\\"title\\\":\\\"Custom\\\"}]}],\\\"skills\\\":[\\\"Php\\\",\\\"WordPress\\\",\\\"Laravel\\\",\\\"AWS\\\",\\\"NodeJs\\\",\\\"Python\\\",\\\"Golang\\\",\\\"MongoDb\\\",\\\"PostgresSql\\\",\\\"MySql\\\",\\\"C++\\\"]}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL2RhdGEvSG9tZS5qc29uLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./data/Home.json\n");
+
+/***/ })
+
+})
